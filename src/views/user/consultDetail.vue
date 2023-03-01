c337ee5fd5a1d1a64c9ffe9730e619b47f51c741
former <1633852615@qq.com>
2023-03-01 14:38:13 +0800

    详情按钮处理

--- src/views/user/consultDetail.vue
+++ src/views/user/consultDetail.vue
@@ -75,15 +75,65 @@ onMounted(async () => {
         />
       </van-cell-group>
     </div>
-    <div class="detail-action van-hairline--top">
+    <div class="detail-time" v-if="item.status === OrderType.ConsultPay">
+      <!-- 待支付，倒计时提示 -->
+      请在<van-count-down
+        :time="item.countdown * 1000"
+      />内完成支付，订单超时将取消
+    </div>
+    <div
+      class="detail-action van-hairline--top"
+      v-if="item.status === OrderType.ConsultPay"
+    >
       <div class="price">
         <span>需付款</span>
-        <span>￥39.00</span>
+        <span>￥{{ item.actualPayment.toFixed(2) }}</span>
       </div>
       <van-button type="default" round>取消问诊</van-button>
       <van-button type="primary" round>继续支付</van-button>
     </div>
+    <div
+      class="detail-action van-hairline--top"
+      v-if="item.status === OrderType.ConsultWait"
+    >
+      <van-button type="default" round>取消问诊</van-button>
+      <van-button type="primary" round :to="`/room?orderId=${item.id}`"
+        >继续沟通</van-button
+      >
+    </div>
+    <div
+      class="detail-action van-hairline--top"
+      v-if="item.status === OrderType.ConsultChat"
+    >
+      <van-button type="default" round v-if="item.prescriptionId"
+        >查看处方</van-button
+      >
+      <van-button type="primary" round :to="`/room?orderId=${item.id}`"
+        >继续沟通</van-button
+      >
+    </div>
+    <div
+      class="detail-action van-hairline--top"
+      v-if="item.status === OrderType.ConsultComplete"
+    >
+      <cp-consult-more></cp-consult-more>
+      <van-button type="default" round :to="`/room?orderId=${item.id}`"
+        >问诊记录</van-button
+      >
+      <van-button type="primary" round v-if="item.evaluateId"
+        >写评价</van-button
+      >
+      <van-button type="default" round v-else>查看评价</van-button>
+    </div>
+    <div
+      class="detail-action van-hairline--top"
+      v-if="item.status === OrderType.ConsultCancel"
+    >
+      <van-button type="default" round>删除订单</van-button>
+      <van-button type="primary" round to="/">咨询其他医生</van-button>
+    </div>
   </div>
+  <!-- 待支付，倒计时提示 -->
   <div class="consult-detail-page" v-else>
     <cp-nav-bar title="问诊详情" />
     <van-skeleton title :row="4" style="margin-top: 30px" />
@@ -230,5 +280,21 @@ onMounted(async () => {
 .van-cell {
   padding-left: 18px;
   padding-right: 18px;
+}
+.detail-time {
+  position: fixed;
+  left: 0;
+  bottom: 65px;
+  width: 100%;
+  height: 44px;
+  background-color: #fff7eb;
+  text-align: center;
+  line-height: 44px;
+  font-size: 13px;
+  color: #f2994a;
+  .van-count-down {
+    display: inline;
+    color: #f2994a;
+  }
 }
 </style>
