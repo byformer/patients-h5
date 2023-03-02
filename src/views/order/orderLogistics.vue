d7fb90a0fd8ddc2a10f1c1a8ce48e8fa1eb9bf91
former <1633852615@qq.com>
2023-03-02 15:06:26 +0800

    地图物流轨迹

--- src/views/order/orderLogistics.vue
+++ src/views/order/orderLogistics.vue
@@ -3,6 +3,15 @@ import { getMedicalOrderLogistics } from '@/services/order'
 import type { Logistics } from '@/types/order'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import AMapLoader from '@amap/amap-jsapi-loader'
+import endImg from '@/assets/end.png'
+import startImg from '@/assets/start.png'
+import carImg from '@/assets/car.png'
+
+// v2.0需要配置
+window._AMapSecurityConfig = {
+  securityJsCode: '415e917da833efcf2d5b69f4d821784b'
+}
 
 // 获取物流信息
 const logistics = ref<Logistics>()
@@ -10,6 +19,67 @@ const route = useRoute()
 onMounted(async () => {
   const res = await getMedicalOrderLogistics(route.params.id as string)
   logistics.value = res.data
+
+  AMapLoader.load({
+    key: '4eed3d61125c8b9c168fc22414aaef7e',
+    version: '2.0'
+  }).then((AMap) => {
+    // 使用 Amap 初始化地图
+    const map = new AMap.Map('map', {
+      mapStyle: 'amap://styles/whitesmoke',
+      zoom: 12
+    })
+    AMap.plugin('AMap.Driving', function () {
+      const driving = new AMap.Driving({
+        map: map,
+        showTraffic: false,
+        hideMarkers: true
+      })
+
+      // 起点
+      const start = res.data.logisticsInfo.shift()
+      const startMarker = new AMap.Marker({
+        position: [start?.longitude, start?.latitude],
+        icon: startImg
+      })
+      map.add(startMarker)
+      // 终点
+      const end = res.data.logisticsInfo.pop()
+      const endMarker = new AMap.Marker({
+        position: [end?.longitude, end?.latitude],
+        icon: endImg
+      })
+      map.add(endMarker)
+
+      driving.search(
+        [start?.longitude, start?.latitude],
+        [end?.longitude, end?.latitude],
+        {
+          waypoints: res.data.logisticsInfo.map((item) => [
+            item.longitude,
+            item.latitude
+          ])
+        },
+        (status: string, result: object) => {
+          // 未出错时，result即是对应的路线规划方案
+          console.log(status, result)
+          const marker = new AMap.Marker({
+            icon: carImg,
+            position: [
+              res.data.currentLocationInfo.longitude,
+              res.data.currentLocationInfo.latitude
+            ],
+            anchor: 'center'
+          })
+          map.add(marker)
+          setTimeout(() => {
+            map.setFitView([marker])
+            map.setZoom(10)
+          }, 3000)
+        }
+      )
+    })
+  })
 })
 </script>
 
